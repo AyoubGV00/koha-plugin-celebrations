9c8f1aae652c5bac9079f433a0bd5059ab6bd0f4
Ayoub Glizi-Vicioso <ayoub.glizi-vicioso@inLibro.com>
2026-01-28 15:39:59 -0500

    Revert "Ajout d'un nouveau thème pour le Nouvel An chinois"
    This reverts commit 0ff8e9bcb9a4bb7c1f8aebc7138d841b27393c5c.

--- Koha/Plugin/Celebrations/js/nouvel-an-chinois/background-chinese-new-year.js
+++
@@ -1,9 +0,0 @@
-fetch('/api/v1/contrib/Celebrations-api/static/images/background-chinese-new-year.jpg')
-  .then(res => res.blob())
-  .then(blob => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      document.body.style.backgroundImage = `url('${reader.result}')`;
-    };
-    reader.readAsDataURL(blob);
-  });
